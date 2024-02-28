3b315d792e09df1f6709bca02f1d9a15f774e82c
Ilkhomjon2002 <ilhommaxsudov3@gmail.com>
2024-02-28 22:50:45 +0900

    searchInsertBinary

--- searchInsert.js
+++ searchInsert.js
@@ -4,3 +4,19 @@ var searchInsert = function (nums, target) {
 		? nums.findIndex((val) => val == target)
 		: nums.findIndex((val) => val > target);
 };
+
+// with binary search
+
+var searchInsertBinary = function (nums, target) {
+	let low = 0;
+	let high = nums.length - 1;
+	let mid;
+
+	while (low <= high) {
+		mid = low + Math.floor((high - low) / 2);
+		if (target === nums[mid]) return mid;
+		else if (target > nums[mid]) low = mid + 1;
+		else high = mid - 1;
+	}
+	return low;
+};
